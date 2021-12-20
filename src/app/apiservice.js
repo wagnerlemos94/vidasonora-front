e5d9a3c70547982cb49a30fb7a6ab9f69d7dbc09
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-12-20 16:02:28 -0300

    ajusta url

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL:'http://localhost:8080'
-    // baseURL:'https://vidasonora-api.herokuapp.com'
+    // baseURL:'http://localhost:8080'
+    baseURL:'https://vidasonora-api.herokuapp.com'
 });
 
 
